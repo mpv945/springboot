39283d536599ef4959c538e86d5f065f405a3275
xiehaijun <zhenxindefeng@126.com>
2018-09-13 16:02:59 +0800

    创建项目

---
+++ batchJob/src/main/java/org/haijun/study/batchJob/BatchJobApplication.java
@@ -0,0 +1,16 @@
+package org.haijun.study.batchJob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchJobApplication {
+
+    /**
+     * 第一个spring boot项目
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(BatchJobApplication.class, args);
+    }
+}
---
+++ batchJob/src/test/java/org/haijun/study/batchJob/BatchJobApplicationTests.java
@@ -0,0 +1,19 @@
+package org.haijun.study.batchJob;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BatchJobApplicationTests {
+
+    /**
+     * 第一个spring boot测试类
+     */
+    @Test
+    public void contextLoads() {
+    }
+
+}
